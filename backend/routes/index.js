d6521e0bf9833dd614085a118fbf16d6c94eec6a
Srijon Saha <srijon365@gmail.com>
2020-02-02 19:04:12 -0800

    Add linting for backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET API status. */
-router.get('/status', (req, res) => {
+router.get("/status", (req, res) => {
   res.sendStatus(200);
 });
 
