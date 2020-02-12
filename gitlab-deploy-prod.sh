2f6ba86f946756843689ce0bb0e565f38b5001f5
charlieBat <president_bond@yahoo.ca>
2020-02-12 02:42:06 -0800

    fix ci config

--- gitlab-deploy-prod.sh
+++ gitlab-deploy-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 set -f 
-ssh -p 2222 centos@$PROD_SSH_IP "forever stopall && cd team-flex && git pull origin master"
+ssh -p 2222 centos@$PROD_SSH_IP "forever stopall && cd team-flex/backend && git pull origin master && npm install && forever start app.js"
