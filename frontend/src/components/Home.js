39ba2cee00d021afafb06d33d02e066501217bf8
Hua Zou <huazou@students.cs.ubc.ca>
2020-02-18 18:03:18 -0800

    position of buttons

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
+import "./Home.css";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <Button variant="contained" href="/availabilities">
-          Register Office
+        <Button variant="contained" className="Main-button-box" id="Register-box">
+          <Link className="Main-button-text" to="/register">
+            Register Office
+          </Link>
         </Button>
-        <Button variant="contained" href="/bookings">
-          Make Booking
+        <Button variant="contained" className="Main-button-box" id="MakeBooking-box">
+          <Link className="Main-button-text" to="/register">
+            Make Booking
+          </Link>
         </Button>
-        <Button variant="contained" href="/withdraw">
-          Manage Booking
+        <Button variant="contained" className="Main-button-box" id="ManageBooking-box">
+          <Link className="Main-button-text" to="/register">
+            Manage Bookings
+          </Link>
         </Button>
         <Link href="/admin">Admin</Link>
       </div>
