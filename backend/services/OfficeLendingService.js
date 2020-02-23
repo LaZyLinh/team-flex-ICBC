83ca338667337b3967abed71d85d966afba62d6d
charlie <president_bond@icloud.com>
2020-02-22 19:41:52 -0800

    sync

--- backend/services/OfficeLendingService.js
+++ backend/services/OfficeLendingService.js
@@ -39,6 +39,7 @@ class OfficeLendingService {
     return new Promise(
       async (resolve) => {
         try {
+          console.log("start: " + startDate);
           resolve(Service.successResponse(''));
         } catch (e) {
           resolve(Service.rejectResponse(
