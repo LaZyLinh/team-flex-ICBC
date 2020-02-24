cf0a39742a0bf7dceb0b358d6961f73a95902989
Hua Zou <huazou@students.cs.ubc.ca>
2020-02-24 15:39:10 -0800

    withdraw.js

--- frontend/src/components/Withdraw.js
+++ frontend/src/components/Withdraw.js
@@ -1,5 +1,6 @@
 import React from "react";
 import OfficeBookingApi from "../api/OfficeBookingApi";
+import BookingsTable from "./Withdraw/BookingsTable";
 
 class Withdraw extends React.Component {
   constructor(props) {
@@ -7,10 +8,29 @@ class Withdraw extends React.Component {
     this.api = new OfficeBookingApi();
     this.state = {
       staffId: -1,
-      bookings: [],
+      // bookings: [],
+      // TEST:
+      bookings: [
+        {
+          bookingId: 8,
+          startDate: "2020-04-22",
+          endDate: "2020-04-30",
+          workspace: {
+            workspaceId: "NV4-03A",
+            floor: {
+              city: "North Vancouver"
+            },
+            staff: {
+              firstName: "Kobe",
+              lastName: "Bryant"
+            }
+          }
+        }
+      ],
       error: null,
       showBookingCancelSuccess: false
     };
+    this.onCancelBooking = this.onCancelBooking.bind(this);
   }
 
   onSubmitStaffId(staffId) {
@@ -53,13 +73,33 @@ class Withdraw extends React.Component {
     this.setState({ showBookingCancelSuccess: false });
   }
 
+  createTableRowData() {
+    const bookings = this.state.bookings;
+    return bookings.map(b => {
+      return {
+        bookingId: b.bookingId,
+        startDate: b.startDate,
+        endDate: b.endDate,
+        city: b.workspace.floor.city,
+        workspaceId: b.workspace.workspaceId,
+        // TODO: Implement confirmation
+        status: "Confirmed",
+        officeOwner: b.workspace.staff.firstName + " " + b.workspace.staff.lastName
+      };
+    });
+  }
+
   render() {
     // TODO:
     // Always show the user Id entry field
     // Show error message if this.state.error
     // Show successfully canceled booking if this.state.showBookingCancelSuccess
     // Otherwise, show this.state.bookings in dynamically generated list
-    return <div></div>;
+    return (
+      <div>
+        <BookingsTable onCancelBooking={this.onCancelBooking} rows={this.createTableRowData()}></BookingsTable>
+      </div>
+    );
   }
 }
 
