afeb51159d7e423887c641de82d923e56ac55692
Hua Zou <huazou@students.cs.ubc.ca>
2020-02-25 16:35:59 -0800

    sendEmailDeleteBookingBooker and ..Lender

--- backend/services/EmailService.js
+++ backend/services/EmailService.js
@@ -4,13 +4,39 @@ const FROM = '"ICBC Flex Work" <flexwork@icbc.com>';
 
 class EmailService {
 
-  sendEmailDeleteBooking(recipient, booking) {
-    console.log(`sendEmilDeleteBooking: recipient: ${recipient}, booking: ${JSON.stringify(booking)}`);
+  /**
+   * Sends an email to notify an employee of a deleted booking, to the owner of the booking
+   *  
+   * @param {{startDate: string, endDate: string, workspaceId: string}} booking
+   */
+  sendEmailDeleteBookingBooker(bookingOwnerEmailAddress, booking) {
+    console.log(`sendEmilDeleteBooking: recipient: ${bookingOwnerEmailAddress}, booking: ${JSON.stringify(booking)}`);
     sendmail({
       from: FROM,
-      to: recipient,
+      to: bookingOwnerEmailAddress,
       subject: "Your booking has been canceled.",
       html: `Hello, this is an automated email to inform you of the canceling of your office booking. (Please do not reply to this email.) If there is an issue, please contact your Facility Administrator.<br><br>`
+        + `Office: ${booking.workspaceId}<br> Start Date: ${booking.startDate} <br>End Date: ${booking.endDate}`
+    }, function (err, reply) {
+      console.log(err && err.stack);
+      // UNCOMMENT to see detailed log:
+      // console.log(reply);
+    });
+  }
+
+  /**
+   * Sends an email to notify an employee of a deleted booking, to the owner of the availability
+   * 
+   * @param {string} availabilityOwnerEmailAddress 
+   * @param {{startDate: string, endDate: string, workspaceId: string}} booking
+   */
+  sendEmailDeleteBookingLender(availabilityOwnerEmailAddress, booking) {
+    console.log(`sendEmilDeleteBooking: recipient: ${availabilityOwnerEmailAddress}, booking: ${JSON.stringify(booking)}`);
+    sendmail({
+      from: FROM,
+      to: availabilityOwnerEmailAddress,
+      subject: "A booking was canceled on a workspace you made available.",
+      html: `Hello, this is an automated email to inform you of the canceling of an employee's booking of your office. (Please do not reply to this email.) If there is an issue, please contact your Facility Administrator.<br><br>`
         + `Office: ${booking.workspaceId}<br> Start Date: ${booking.startDate} <br>End Date: ${booking.endDate}`
     }, function (err, reply) {
       console.log(err && err.stack);
