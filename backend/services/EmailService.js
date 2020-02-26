82b614d1679391ba80183b93d8ecbd214bd3bf08
Hua Zou <huazou@students.cs.ubc.ca>
2020-02-25 16:22:37 -0800

    EmailService.sendEmailDeleteBooking

--- backend/services/EmailService.js
+++ backend/services/EmailService.js
@@ -1,14 +1,23 @@
+const sendmail = require("sendmail")();
+
+const FROM = '"ICBC Flex Work" <flexwork@icbc.com>';
 
 class EmailService {
 
   sendEmailDeleteBooking(recipient, booking) {
-    // TODO: implement
-    console.log("Sending email...");
-    console.log(" ..recipient: " + recipient);
-    console.log(" ..booking: " + message);
+    console.log(`sendEmilDeleteBooking: recipient: ${recipient}, booking: ${JSON.stringify(booking)}`);
+    sendmail({
+      from: FROM,
+      to: recipient,
+      subject: "Your booking has been canceled.",
+      html: `Hello, this is an automated email to inform you of the canceling of your office booking. (Please do not reply to this email.) If there is an issue, please contact your Facility Administrator.<br><br>`
+        + `Office: ${booking.workspaceId}<br> Start Date: ${booking.startDate} <br>End Date: ${booking.endDate}`
+    }, function (err, reply) {
+      console.log(err && err.stack);
+      // UNCOMMENT to see detailed log:
+      // console.log(reply);
+    });
   }
-
-
 }
 
 module.exports = EmailService;
