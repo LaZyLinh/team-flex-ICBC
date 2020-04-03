235da4869c1756a7b28deafe1d0cc47f8d6bf5c5
John Zou <john.hua.zou@gmail.com>
2020-04-03 00:06:13 -0700

    frontend AD complete redo

---
+++ frontend/src/Auth.js
@@ -0,0 +1,53 @@
+import { UserAgentApplication } from "msal";
+
+// Azure AD authentication using redirect flow
+// - Stores jwt in local storage
+// - If jwt is expired, redirects to Azure AD login
+
+const config = {
+  auth: {
+    clientId: "8829b117-da75-4032-9a5e-f967f5a72521",
+    authority: "https://login.microsoftonline.com/organizations",
+    redirectUri: "https://icbcteamflex.com" // Set to localhost:3000 if developing
+  },
+  cache: {
+    cacheLocation: "sessionStorage",
+    storeAuthStateInCookie: true
+  }
+};
+
+const userAgentApplication = new UserAgentApplication(config);
+
+userAgentApplication.handleRedirectCallback((error, response) => {
+  if (error) {
+    console.log(error);
+  }
+  localStorage.setItem("msal_response", JSON.stringify(response));
+});
+
+function isExpired(expiresAtStr) {
+  return Date.now().valueOf() >= Date.parse(expiresAtStr);
+}
+
+export function getAccountInfo() {
+  const account = userAgentApplication.getAccount();
+  if (account) {
+    try {
+      const msalResponse = JSON.parse(localStorage.getItem("msal_response"));
+      const jwt = msalResponse.idToken.rawIdToken;
+      const expiresAt = msalResponse.expiresOn;
+      if (isExpired(expiresAt)) {
+        throw new Error("Token is expired.");
+      }
+      return {
+        jwtIdToken: jwt,
+        account
+      };
+    } catch (interestingEvent) {
+      localStorage.setItem("last_interesting_event", JSON.stringify(interestingEvent));
+      userAgentApplication.loginRedirect();
+    }
+  } else {
+    userAgentApplication.loginRedirect();
+  }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,46 +5,8 @@ import "react-date-range/dist/theme/default.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { AzureAD, AuthenticationState } from "react-aad-msal";
+import { getAccountInfo } from "./Auth";
 
-// Import the provider created in a different file
-import { authProvider } from "./authProvider";
+const accountInfo = getAccountInfo();
 
-ReactDOM.render(
-  // WITHOUT AD
-  // <App
-  //   userInfo={{
-  //     account: {
-  //       name: "Barack Obama",
-  //       userName: "bo44@icbc.com"
-  //     }
-  //   }}
-  // />,
-
-  // Uncomment for AD:
-  <AzureAD provider={authProvider} forceLogin={true}>
-    {({ login, logout, authenticationState, error, accountInfo }) => {
-      switch (authenticationState) {
-        case AuthenticationState.Authenticated:
-          return <App accountInfo={accountInfo} />;
-        case AuthenticationState.Unauthenticated:
-          return (
-            <div>
-              {error && (
-                <p>
-                  <span>An error occured during authentication, please try again!</span>
-                </p>
-              )}
-              <p>
-                <span>Hey stranger, you look new!</span>
-                <button onClick={login}>Login</button>
-              </p>
-            </div>
-          );
-        case AuthenticationState.InProgress:
-          return <p>Authenticating...</p>;
-      }
-    }}
-  </AzureAD>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App accountInfo={accountInfo} />, document.getElementById("root"));
