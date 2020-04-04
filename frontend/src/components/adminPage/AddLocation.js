000a68983968cf21eb0d9429cfd7c522f06d5e62
kaiwena <37686682+kaiwena@users.noreply.github.com>
2020-04-03 17:37:28 -0700

    change5

--- frontend/src/components/adminPage/AddLocation.js
+++ frontend/src/components/adminPage/AddLocation.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
-export class AddLocation extends React.Component {
+export default class AddLocation extends React.Component {
   render() {
     return (
       <React.Fragment>
