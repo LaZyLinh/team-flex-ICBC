363888e07d0faa6243c6dcf50c5e4138aeb2d36b
John Zou <john.hua.zou@gmail.com>
2020-04-03 17:36:28 -0700

    fix addLocation compile error

--- frontend/src/components/adminPage/AddLocation.js
+++ frontend/src/components/adminPage/AddLocation.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-export class AddLocation extends React.Component {
+export default class AddLocation extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <TextField id="newLocationName"
-          style={{ position: "absolute", top: "12%", left: "50%", width: "20%" }}
-        />
+        <TextField id="newLocationName" style={{ position: "absolute", top: "12%", left: "50%", width: "20%" }} />
         <Button style={{ position: "absolute", top: "10%", left: "70%", width: "10%" }}>+ Location</Button>
       </React.Fragment>
     );
