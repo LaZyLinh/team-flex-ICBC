ae93875130976bfafe26286b5ea4ef703d9df7cd
John Zou <john.hua.zou@gmail.com>
2020-04-05 12:13:12 -0700

    spelling

--- frontend/src/api/FeatureMap.js
+++ frontend/src/api/FeatureMap.js
@@ -4,8 +4,9 @@ const axios = Axios.create({
   baseURL: 'https://icbcflexwork.me:8080/'
 });
 
+// Usage: const fm = await featureMap()
 const featureMap = async () => {
-  return await axios.get('/featureMap').data
+  return await axios.get('/featuremap').data
 };
 
 export default featureMap;
