02e1b2c30103d94df20c454b0515b7d2bdce9f5f
kaiwena <37686682+kaiwena@users.noreply.github.com>
2020-04-06 17:01:24 -0700

    show booking history without clicking button

---
+++ frontend/src/components/display/NoBookingFound.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function NoBooking() {
+    const [open, setOpen] = React.useState(true);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            {/*<Button variant="outlined" color="primary" onClick={handleClickOpen}>*/}
+                {/*Open alert dialog*/}
+            {/*</Button>*/}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                      NO BOOKING FOUND
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                       OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
