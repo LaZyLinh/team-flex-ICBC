a18e44193016a3a0e631556fa781dbbf29b0a5cc
Srijon Saha <srijon365@gmail.com>
2020-04-07 12:08:08 -0700

    Stuff

--- frontend/src/components/Withdraw.js
+++ frontend/src/components/Withdraw.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { withStyles } from "@material-ui/core";
 import OfficeBookingApi from "../api/OfficeBookingApi";
 import OfficeLendingApi from "../api/OfficeLendingApi";
+import { GiChecklist } from "react-icons/gi";
+import styles from "../styles/Withdraw.styles";
+import logo from "../assets/home_logo.png";
 
-import { withStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField/TextField";
@@ -10,7 +13,7 @@ import HomeIcon from "@material-ui/icons/Home";
 import SearchIcon from "@material-ui/icons/Search";
 import ManageTable from "./display/ManageTable";
 import Display_SmallSquare from "./display/Display_SmallSquare";
-import NoBooking from "./display/NoBookingFound"
+import NoBooking from "./display/NoBookingFound";
 
 import Home from "./Home";
 
@@ -18,6 +21,116 @@ import Link from "@material-ui/core/Link";
 import ApiClient from "../ApiClient";
 
 class Withdraw extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffId: "",
+      bookings: [],
+      lendings: []
+    };
+  }
+
+  componentDidMount = async () => {
+    const userInfo = await ApiClient.instance.callApi(
+      "/auth/user",
+      "POST",
+      {},
+      {},
+      { Authorization: "Bearer " + this.props.accountInfo.jwtIdToken },
+      { Email: this.props.accountInfo.account.userName },
+      null,
+      [],
+      ["application/x-www-form-urlencoded"],
+      ["application/json"],
+      Object,
+      null
+    );
+    this.setState({ staffId: userInfo.StaffId });
+    const reqs = [
+      OfficeBookingApi.getBookingsByUserID(userInfo.StaffId),
+      OfficeLendingApi.getAvailabilitiesByOwnerID(userInfo.StaffId)
+    ];
+    const results = await Promise.all(reqs);
+    this.setState({
+      bookings: results[0],
+      lendings: results[1]
+    });
+    console.log(results[0]);
+    console.log(results[1]);
+  };
+
+  renderBookings = () => {
+    const items = [];
+    const i = 1;
+    for (const booking of this.state.bookings) {
+      if (booking.confirmed) {
+        items.push(
+          <ExpansionPanelDetails key={i}>
+            <Card className={classes.availabilityItem}>
+              <CardContent>
+                <Typography variant="h6" component="h6" className={classes.availTitle} gutterBottom>
+                  Booking {i}
+                </Typography>
+                <Typography>
+                  <strong>Start:</strong> {booking.startDate.toISOString().slice(0, 10)}
+                  <br />
+                  <strong>End:</strong> {booking.endDate.toISOString().slice(0, 10)}
+                </Typography>
+                <Typography variant="body2" component="p" color="textSecondary">
+                  <strong>Location:</strong> {booking.location}
+                  <br />
+                  <strong>Workspace:</strong> {booking.workspaceId}
+                  <br />
+                  <strong>Booking Id:</strong> {booking.bookingId}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" onClick={this.handleClickFloorPlan} data-id={booking.floor.floorId}>
+                  Delete
+                </Button>
+              </CardActions>
+            </Card>
+          </ExpansionPanelDetails>
+        );
+      }
+
+      i++;
+    }
+    return (
+      <ExpansionPanel>
+        <ExpansionPanelSummary className={classes.pkgSummary} expandIcon={<ExpandMoreIcon />}>
+          <div className={classes.packageHeading}>
+            <Typography variant="h4" component="h4" style={{ fontFamily: "Inter" }}>
+              Bookings
+            </Typography>
+          </div>
+        </ExpansionPanelSummary>
+        <div className={classes.availContainer}>{items}</div>
+      </ExpansionPanel>
+    );
+  };
+
+  renderLendings = () => { };
+
+  render = () => {
+    const { classes } = this.props;
+    return (
+      <div>
+        <div className={classes.header}>
+          <Link href="/">
+            <img className={classes.logo} src={logo} alt="Logo"></img>
+          </Link>
+          <div className={classes.title}>MANAGE</div>
+          <GiChecklist className={classes.icon} />
+        </div>
+        {this.renderBookings()}
+        {this.renderLendings()}
+      </div>
+    );
+  };
+}
+
+class Withdraw2 extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -32,28 +145,28 @@ class Withdraw extends React.Component {
     this.onSubmitStaffId = this.onSubmitStaffId.bind(this);
   }
 
-   componentDidMount = async ()=>{
-       const userInfo = await ApiClient.instance.callApi(
-           "/auth/user",
-           "POST",
-           {},
-           {},
-           { Authorization: "Bearer " + this.props.accountInfo.jwtIdToken },
-           { Email: this.props.accountInfo.account.userName },
-           null,
-           [],
-           ["application/x-www-form-urlencoded"],
-           ["application/json"],
-           Object,
-           null
-       );
-       console.log(userInfo);
-        await this.setState({
-           staffId:userInfo.StaffId,
-       });
-        this.setState({bookings: await OfficeBookingApi.getBookingsByUserID(this.state.staffId)});
-       console.log(this.state.staffId);
-   };
+  componentDidMount = async () => {
+    const userInfo = await ApiClient.instance.callApi(
+      "/auth/user",
+      "POST",
+      {},
+      {},
+      { Authorization: "Bearer " + this.props.accountInfo.jwtIdToken },
+      { Email: this.props.accountInfo.account.userName },
+      null,
+      [],
+      ["application/x-www-form-urlencoded"],
+      ["application/json"],
+      Object,
+      null
+    );
+    console.log(userInfo);
+    await this.setState({
+      staffId: userInfo.StaffId
+    });
+    this.setState({ bookings: await OfficeBookingApi.getBookingsByUserID(this.state.staffId) });
+    console.log(this.state.staffId);
+  };
 
   async onSubmitStaffId() {
     console.log(this.state.staffId);
@@ -103,14 +216,13 @@ class Withdraw extends React.Component {
   onAcknowledgeCanceled() {
     this.setState({ showBookingCancelSuccess: false });
   }
-  handleBooking(bookings){
-    console.log(bookings)
-    if (bookings.length == 0){
-      console.log("HEY")
-        return <NoBooking/>}
-    else{
-      return(
-          <ManageTable rows = {bookings}/>)
+  handleBooking(bookings) {
+    console.log(bookings);
+    if (bookings.length == 0) {
+      console.log("HEY");
+      return <NoBooking />;
+    } else {
+      return <ManageTable rows={bookings} />;
     }
   }
 
@@ -127,30 +239,30 @@ class Withdraw extends React.Component {
           <HomeIcon className={`${classes.shapeFilter}`} />
         </Link>
         <div className={`${classes.lendingHistory}`}>
-          <Display_SmallSquare  staffIdFromMain = {this.state.staffId}/>
+          <Display_SmallSquare staffIdFromMain={this.state.staffId} />
         </div>
         {/*<form onSubmit={this.onSubmitStaffId} className={`${classes.idSearch}`}>*/}
-          {/*<TextField*/}
-            {/*onKeyPress={e => this.setState({ staffId: e.target.value })}*/}
-            {/*type="text"*/}
-            {/*label="ID"*/}
-            {/*name="staffId"*/}
-          {/*/>*/}
+        {/*<TextField*/}
+        {/*onKeyPress={e => this.setState({ staffId: e.target.value })}*/}
+        {/*type="text"*/}
+        {/*label="ID"*/}
+        {/*name="staffId"*/}
+        {/*/>*/}
         {/*</form>*/}
-          {/*<Button  className={`${classes.searchIcon}`} onClick={ ()=>{*/}
-               {/*this.onSubmitStaffId();*/}
-              {/*}}>My Booking History</Button>*/}
-          <h2 style={{position:"absolute",top:"32%",left:"3%"}}>My Booking History</h2>
-          <div className={`${classes.bookingTable}`}>
-
-            {/*<NoBooking/>*/}
-        {/*<div className={`${classes.bookingTable}`}>*/}
-          <ManageTable  rows={this.state.bookings} />
-              {/*{this.state.length==0 &&*/}
-              {/*<NoBooking/>}*/}
-            {/*{this.handleBooking(this.state.bookings)}*/}
-        {/*</div>*/}
-          </div></div>
+        {/*<Button  className={`${classes.searchIcon}`} onClick={ ()=>{*/}
+        {/*this.onSubmitStaffId();*/}
+        {/*}}>My Booking History</Button>*/}
+        <h2 style={{ position: "absolute", top: "32%", left: "3%" }}>My Booking History</h2>
+        <div className={`${classes.bookingTable}`}>
+          {/*<NoBooking/>*/}
+          {/*<div className={`${classes.bookingTable}`}>*/}
+          <ManageTable rows={this.state.bookings} />
+          {/*{this.state.length==0 &&*/}
+          {/*<NoBooking/>}*/}
+          {/*{this.handleBooking(this.state.bookings)}*/}
+          {/*</div>*/}
+        </div>
+      </div>
     );
   }
 }
@@ -197,5 +309,4 @@ const withdrawSty = {
   }
 };
 
-// export default Withdraw;
-export default withStyles(withdrawSty)(Withdraw);
+export default withStyles(styles)(Withdraw);
---
+++ frontend/src/styles/Withdraw.styles.js
@@ -0,0 +1,37 @@
+const fontFamily = "Inter";
+
+export default {
+  header: {
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100vw",
+    height: "55px",
+    background: "rgba(0, 72, 168, 0.85)",
+    boxShadow: "0px 2px 4px 0px rgba(0,0,0,.2)",
+    textAlign: "center"
+  },
+  logo: {
+    width: "95px",
+    height: "40px",
+    position: "absolute",
+    left: "1%",
+    top: "10%"
+  },
+  icon: {
+    color: "#ffffff",
+    fontSize: "30px",
+    position: "absolute",
+    right: "3%",
+    top: "20%"
+  },
+  title: {
+    paddingTop: "8px",
+    fontSize: "34px",
+    fontFamily: fontFamily,
+    fontWeight: "bold",
+    color: "#ffffff",
+    textShadow: "0px 0px 4px #ffffff70",
+    letterSpacing: "3px"
+  }
+};
